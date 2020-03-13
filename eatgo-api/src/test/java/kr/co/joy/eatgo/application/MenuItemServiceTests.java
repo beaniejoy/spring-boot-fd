ff6268f6966aead6bac3b3def290be428c72273d
hanbinleejoy <lhb1025@gmail.com>
2020-03-14 04:27:21 +0900

    200314 MenuItems Inf. Update 2

---
+++ eatgo-api/src/test/java/kr/co/joy/eatgo/application/MenuItemServiceTests.java
@@ -0,0 +1,39 @@
+package kr.co.joy.eatgo.application;
+
+import kr.co.joy.eatgo.domain.MenuItem;
+import kr.co.joy.eatgo.domain.MenuItemRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class MenuItemServiceTests {
+    private MenuItemService menuItemService;
+
+    @Mock
+    private MenuItemRepository menuItemRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        menuItemService = new MenuItemService(menuItemRepository);
+    }
+
+    @Test
+    public void bulkUpdate() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(MenuItem.builder().name("Kimchi").build());
+        menuItems.add(MenuItem.builder().name("Gukbob").build());
+        menuItemService.bulkUpdate(1L, menuItems);
+
+        verify(menuItemRepository, times(2)).save(any());
+    }
+}
