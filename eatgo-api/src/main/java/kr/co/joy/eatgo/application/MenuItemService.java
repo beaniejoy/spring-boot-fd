a23975b3b88ce38d72780607cc002dde6d1e4f33
hanbinleejoy <lhb1025@gmail.com>
2020-03-13 20:18:51 +0900

    200313 MenuItems Inf. Update 1

---
+++ eatgo-api/src/main/java/kr/co/joy/eatgo/application/MenuItemService.java
@@ -0,0 +1,14 @@
+package kr.co.joy.eatgo.application;
+
+import kr.co.joy.eatgo.domain.MenuItem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
+        // TODO: bulk update
+    }
+}
