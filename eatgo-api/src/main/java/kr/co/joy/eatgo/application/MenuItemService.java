077c1c7a93ab3fabb74c6220063b5d7e4ede5c14
hanbinleejoy <lhb1025@gmail.com>
2020-03-14 04:27:39 +0900

    200314 MenuItems Inf. Update 2

--- eatgo-api/src/main/java/kr/co/joy/eatgo/application/MenuItemService.java
+++ eatgo-api/src/main/java/kr/co/joy/eatgo/application/MenuItemService.java
@@ -1,14 +1,25 @@
 package kr.co.joy.eatgo.application;
 
 import kr.co.joy.eatgo.domain.MenuItem;
+import kr.co.joy.eatgo.domain.MenuItemRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class MenuItemService {
+
+    @Autowired
+    private MenuItemRepository menuItemRepository;
 
     public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
         // TODO: bulk update
+        for (MenuItem menuItem : menuItems) {
+            menuItem.setRestaurantId(restaurantId);
+            menuItemRepository.save(menuItem);
+        }
     }
 }
--- eatgo-api/src/test/java/kr/co/joy/eatgo/application/RestaurantServiceTests.java
+++ eatgo-api/src/test/java/kr/co/joy/eatgo/application/RestaurantServiceTests.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
-class RestaurantServiceTest {
+class RestaurantServiceTests {
 
     private RestaurantService restaurantService;
     @Mock
