bc8f0c968f95cf087f5b2b3fea14e5b980b85c51
hanbinleejoy <lhb1025@gmail.com>
2020-03-22 17:07:45 +0900

    200322 Project admin-api Separate

--- eatgo-admin-api/src/main/java/kr/co/joy/eatgo/EatgoAdminApiApplication.java
+++ eatgo-admin-api/src/main/java/kr/co/joy/eatgo/EatgoAdminApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EatgoApplication {
+public class EatgoAdminApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EatgoApplication.class, args);
+		SpringApplication.run(EatgoAdminApiApplication.class, args);
 	}
 
 }
