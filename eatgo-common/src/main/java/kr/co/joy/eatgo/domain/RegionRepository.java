a3bc897c105f0cf1591e13eb2d34791ca422da4f
hanbinleejoy <lhb1025@gmail.com>
2020-03-26 16:12:51 +0900

    200326 Restaurant Filtering by Region 2

--- eatgo-common/src/main/java/kr/co/joy/eatgo/domain/RegionRepository.java
+++ eatgo-common/src/main/java/kr/co/joy/eatgo/domain/RegionRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface RegionRepository extends CrudRepository<Region, Long> {
+
     List<Region> findAll();
 }
